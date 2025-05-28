f10a2907fa9a23eb7cf72cb84d3d8ad002c1f0f1
Michael Soh <2767724+sohmc@users.noreply.github.com>
2025-05-28 23:46:03 +0000

    Added education filter

--- .ci/config-builder.bash
+++ .ci/config-builder.bash
@@ -1,12 +1,10 @@
 #!/bin/bash -ev
 
-JPATH='data.sections.volunteer.items'
-JPATH='volunteer'
-BUILT_YAML="$(jq -rc --arg jpath $JPATH '.data.sections[$jpath]?.items | [map(select(.visible == true))[] | {layout: "left", title: .position, subtitle: .organization, link: .url.href, link_text: .url.label, caption: .date, quote: .summary}]' resume.json | yq -p=json -o=yaml | sed -e 's/^/      /')"
+SECTION=$1
+BUILT_YAML="$(jq -rc -f .ci/jq-filters/$SECTION resume.json | yq -p=json -o=yaml | sed -e 's/^/      /')"
 
-echo "$BUILT_YAML"
-
-VARIABLE_NAME='VOLUNTEERISM'
+VARIABLE_NAME=$2
 sed -i -e "s#\$${VARIABLE_NAME}#\\n${BUILT_YAML//$'\n'/\\n}#" ./_config.yml
 
 cat ./_config.yml
+
